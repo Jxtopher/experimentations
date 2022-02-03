6b26fff9f01f94635fd01b04df12044c14f2da72
Jxtopher <39927513+Jxtopher@users.noreply.github.com>
2022-02-04 00:22:28 +0100

    Update game_of_life.py

--- Game of life/game_of_life.py
+++ Game of life/game_of_life.py
@@ -1,4 +1,6 @@
 # See: https://conwaylife.com
+from logging import exception
+from click import pass_context
 import matplotlib.pyplot as plt
 import numpy as np
 import abc
@@ -20,6 +22,9 @@ class Matrix:
         num_line = index // self.size
         num_col = index - num_line * self.size
         return num_line, num_col
+
+    def get_size(self) -> int:
+        return self.size
 
     def to_index(self, x: int, y: int) -> int:
         return x * self.size + y
@@ -127,6 +132,62 @@ class Game_of_life(Cellular_automaton):
             else:
                 self.matrix.set_cell(cell, self.state["DED"])
 
+    def load_pattern_RLE(self, rle: str):
+        for cell in range(self.matrix.get_number_of_cells()):
+            self.matrix.set_cell(cell, self.state["DED"])
+
+        x_shift = 3
+        y_shift = 5
+        x = 0
+        y = 0
+        num = ""
+        for c in rle:
+            if c == "$":
+                x = 0
+                y += 1
+            elif (
+                c == "0"
+                or c == "1"
+                or c == "2"
+                or c == "3"
+                or c == "4"
+                or c == "5"
+                or c == "6"
+                or c == "7"
+                or c == "8"
+                or c == "9"
+            ):
+                num += c
+            elif c == "b":
+                if num == "":
+                    x += 1
+                else:
+                    cpt = int(num)
+                    for _ in range(cpt):
+                        x += 1
+                    num = ""
+            elif c == "o":
+                if num == "":
+                    self.matrix.set_cell(
+                        self.matrix.to_index(y_shift + y, x_shift + x),
+                        self.state["ALIVE"],
+                    )
+                    x += 1
+                else:
+                    cpt = int(num)
+                    for _ in range(cpt):
+                        self.matrix.set_cell(
+                            self.matrix.to_index(y_shift + y, x_shift + x),
+                            self.state["ALIVE"],
+                        )
+                        x += 1
+                    num = ""
+            elif c == "!":
+                pass
+            else:
+                raise Exception("[-] Pattern is malformed")
+
+    # rule = B3/S23
     def compute(self):
         for cell in range(self.matrix.get_number_of_cells()):
             neighbors = self.matrix.get_moore(cell)
@@ -150,27 +211,44 @@ class Game_of_life(Cellular_automaton):
         self.cells_new_state.clear()
 
 
-def animation(cellular_automaton: Cellular_automaton):
+def animation(cellular_automaton: Cellular_automaton, savefigs=""):
     fig, ax = plt.subplots(figsize=(6, 6))
+    fig.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=None, hspace=None)
 
+    plt.xticks(np.arange(0, cellular_automaton.matrix.get_size(), 1.0))
+    plt.yticks(np.arange(0, cellular_automaton.matrix.get_size(), 1.0))
     iteration = 0
-    while True:
+    plt.pcolormesh(
+        cellular_automaton.matrix.get_matrix(),
+        cmap=colors.ListedColormap(["black", "red"]),
+    )
+    plt.grid(which="both", color="w", linestyle="-", linewidth=0.1)
+    plt.savefig(savefigs + "-" + str(iteration).zfill(4) + ".png")
+    while iteration < 15:
+        iteration += 1
         ax.cla()
+        plt.xticks(np.arange(0, cellular_automaton.matrix.get_size(), 1.0))
+        plt.yticks(np.arange(0, cellular_automaton.matrix.get_size(), 1.0))
 
         cellular_automaton.compute()
         cellular_automaton.update()
 
-        ax.imshow(
+        plt.pcolormesh(
             cellular_automaton.matrix.get_matrix(),
             cmap=colors.ListedColormap(["black", "red"]),
         )
-        ax.set_axis_off()
-        ax.set_title("frame {}".format(iteration))
-        plt.pause(0.1)
-        iteration += 1
+        # ax.set_axis_off()
+        # ax.set_title("frame {}".format(iteration))
+        plt.grid(which="both", color="w", linestyle="-", linewidth=0.1)
+        if savefigs == "":
+            plt.pause(0.1)
+        else:
+            plt.savefig(savefigs + "-" + str(iteration).zfill(4) + ".png")
 
 
 if __name__ == "__main__":
     # unittest.main()
-    game_of_life = Game_of_life(size=100)
-    animation(game_of_life)
+    game_of_life = Game_of_life(size=13)
+    game_of_life.load_pattern_RLE("3ob3o$o5bo$2bobo$b2ob2o$o5bo!")
+    print(game_of_life.matrix.matrix)
+    animation(game_of_life, "test/xp14_j9d0d9j")
